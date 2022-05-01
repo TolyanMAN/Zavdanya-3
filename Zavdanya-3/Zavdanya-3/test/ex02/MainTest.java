9356708facc05df37e514b74f781b831a1bed2ca
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-01 11:45:38 +0300

    Update MainTest.java

--- Zavdanya-3/Zavdanya-3/test/ex02/MainTest.java
+++ Zavdanya-3/Zavdanya-3/test/ex02/MainTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import ex01.Item2d;
 /** Выполняет тестирование
  * разработанных классов.
- * @author Vlada
+ * @author Maksim
  * @version 2.0
  */
 
@@ -15,27 +15,30 @@ public class MainTest {
     /** Проверка основной функциональности класса {@linkplain ViewResult} */
     @Test
     public void testCalc() {
-        ViewResult view = new ViewResult(5);
-        view.init(5.0);
-        Item2d item = new Item2d();
+              ViewResult view = new ViewResult(5);
+            view.init(5.0);
+                   Item2d item = new Item2d();
         int ctr = 0;
-        item.setOnesNumberAndArguments((byte)7, new double[]{0, 100, 200, 300});
+                     item.setOnesNumberAndArguments((byte)7, new double[]{0, 100, 200, 300});
         assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
                 view.getItems().get(ctr).equals(item));
-        ctr++;
+          ctr++;
+        
+        
         item.setOnesNumberAndArguments((byte)5, new double[]{5, 105, 205, 305});
         assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
                 view.getItems().get(ctr).equals(item));
         ctr++;
-        item.setOnesNumberAndArguments((byte)6, new double[]{10, 110, 210, 310});
+                item.setOnesNumberAndArguments((byte)6, new double[]{10, 110, 210, 310});
         assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
-                view.getItems().get(ctr).equals(item));
+                       view.getItems().get(ctr).equals(item));
         ctr++;
-        item.setOnesNumberAndArguments((byte)5, new double[]{15, 115, 215, 315});
-        assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
+                   item.setOnesNumberAndArguments((byte)5, new double[]{15, 115, 215, 315});
+                assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
                 view.getItems().get(ctr).equals(item));
         ctr++;
         item.setOnesNumberAndArguments((byte)5, new double[]{20, 120, 220, 320});
+        
         assertTrue("expected:<" + item + "> but was:<" + view.getItems().get(ctr) + ">",
                 view.getItems().get(ctr).equals(item));
     }
@@ -44,8 +47,10 @@ public class MainTest {
     public void testRestore() {
         ViewResult view1 = new ViewResult(1000);
         ViewResult view2 = new ViewResult();
+        
         // Вычислим значение функции со случайным шагом приращения аргумента
         view1.init(Math.random()*100.0);
+        
         // Сохраним коллекцию view1.items
         try {
             view1.viewSave();
@@ -60,7 +65,9 @@ public class MainTest {
         }
         // Должны загрузить столько же элементов, сколько сохранили
         assertEquals(view1.getItems().size(), view2.getItems().size());
+        
         // Причем эти элементы должны быть равны.
+        
         // Для этого нужно определить метод equals
         assertTrue("containsAll()", view1.getItems().containsAll(view2.getItems()));
     }
